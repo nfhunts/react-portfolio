4ae603daa257ed058a711e27534d6457a6a5ceea
nfhunts <nfhunts@gmail.com>
2020-01-22 11:25:57 -0700

    added links

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,10 @@ import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home';
 import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import PortfolioDetail from './portfolio/portfolio-detail';
+
 
 export default class App extends Component {
   render() {
@@ -13,19 +17,22 @@ export default class App extends Component {
 
         <Router>
         <div>
+        <h1>Nate Huntsman's Portfolio</h1>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
           <NavigationContainer />
 
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about-me" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/portfolio/:slug" component={PortfolioDetail} />
+
+
           </Switch>
 
         </div>
         </Router>
-
-        <h1>Nate Huntsman's Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-        <PortfolioContainer />
       </div>
     );
   }
