97a2e080348d6aedaf62c19f98d0622bd88c474e
nfhunts <nfhunts@gmail.com>
2020-02-11 18:21:00 -0700

    cleaned up code

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
@@ -18,8 +17,6 @@ export default class App extends Component {
 
         <Router>
         <div>
-        <h1>Nate Huntsman's Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
           <NavigationContainer />
 
           <Switch>
