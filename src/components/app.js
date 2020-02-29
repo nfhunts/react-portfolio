57708ffe5106643097e2b9537064b8e7ef261e6f
nfhunts <nfhunts@gmail.com>
2020-02-29 13:57:39 -0700

    added static login img

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PortfolioContainer from './portfolio/portfolio-container';
+
 import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Blog from './pages/blog';
 import PortfolioDetail from './portfolio/portfolio-detail';
+import Auth from './pages/auth';
 import NoMatch from './pages/no-match';
 
 
@@ -21,6 +22,7 @@ export default class App extends Component {
 
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/auth" component={Auth} />
             <Route path="/about-me" component={About} />
             <Route path="/contact" component={Contact} />
             <Route path="/blog" component={Blog} />
