727b827fcc0710bde553007de493f015de216da5
nfhunts <nfhunts@gmail.com>
2020-02-29 12:56:16 -0700

    update style with react event listener

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,27 +1,49 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { transcode } from "buffer";
 
-export default function(props) {
-        // data that we'll need:
-        // - background image: thumb_image_url
-        // - logo: 
-        // - description: description
-        // - id: id
+export default class PortfolioItem extends Component {
+        constructor(props) {
+            super(props);
 
-        const { id, description, thumb_image_url, logo } = props.item
-    return (
-        <div className="portfolio-item-wrapper">
+            this.state = {
+                portfolioItemClass: ""
+            };
+        }
+
+        handleMouseEnter() {
+            this.setState({portfolioItemClass: 'image-blur'})
+        }
+
+        handleMouseLeave() {
+            this.setState({portfolioItemClass: ""})
+        }
+
+        render() {
+        const { id, description, thumb_image_url, logo_url } = this.props.item;
+           return (
+            <div 
+            className="portfolio-item-wrapper"
+            onMouseEnter={()  => this.handleMouseEnter ()}
+            onMouseLeave={()  => this.handleMouseLeave ()}
+            >
+
             <div
-                className="portfolio-img-background"
+                className={"portfolio-img-background " + this.state.portfolioItemClass}
                 style={{
                     backgroundImage: "url(" + thumb_image_url + ")"
                 }}
             />
 
+          <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+          <img src={logo_url } />
 
-            <img src={logo} />
-            <div>{description}</div>
-            <Link to={`/portfolio/${id}`}>Link</Link>
+          <div className="subtitle">{description}</div>
+          </div>
+          </div>
         </div>
     )
+    }
 }
+
