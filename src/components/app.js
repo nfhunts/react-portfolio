b4fc5f429c7d8a7e21077a00bdba5baa8b03845e
nfhunts <nfhunts@gmail.com>
2020-03-01 16:32:42 -0700

    check if user is logged in

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
@@ -34,9 +35,43 @@ export default class App extends Component {
     });
   }
 
+  checkLoginStatus() {
+    return axios
+      .get("https://api.devcamp.space/logged_in", {
+        withCredentials: true
+      })
+      .then(response => {
+        const loggedIn = response.data.logged_in;
+        const loggedInStatus = this.state.loggedInStatus;
+
+        // If loggedIn and the status is logged LOGGED_IN => return data
+        // If loggedIn status NOT_LOGGED_IN => update state
+        // If not loggedIn and status is LOGGED_IN => update state
+
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
+          });
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
+          });
+        }
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <Router>
           <div>
             <NavigationContainer />
@@ -44,10 +79,10 @@ export default class App extends Component {
             <h2>{this.state.loggedInStatus}</h2>
 
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route exact path='/' component={Home} />
 
               <Route
-                path="/auth"
+                path='/auth'
                 render={props => (
                   <Auth
                     {...props}
@@ -57,12 +92,12 @@ export default class App extends Component {
                 )}
               />
 
-              <Route path="/about-me" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+              <Route path='/about-me' component={About} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/blog' component={Blog} />
               <Route
                 exact
-                path="/portfolio/:slug"
+                path='/portfolio/:slug'
                 component={PortfolioDetail}
               />
               <Route component={NoMatch} />
