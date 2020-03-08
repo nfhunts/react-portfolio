6275d7cac59e6e1b284c42d91691bd12a16ee0be
nfhunts <nfhunts@gmail.com>
2020-03-08 12:52:00 -0600

    finalize sidebar

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://nfhunts.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
-      <div>
-        <h1>PortfolioManager</h1>
-        <h1>PortfolioManager</h1>
+      <div className='portfolio-manager-wrapper'>
+        <div className='left-column'>
+          <h1>Portfolio form....</h1>
+        </div>
+
+        <div className='right-column'>
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
       </div>
     );
   }
