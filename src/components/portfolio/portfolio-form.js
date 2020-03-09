b0b9aed816f075fe361096b2966e01c299243968
nfhunts <nfhunts@gmail.com>
2020-03-08 19:43:51 -0600

    added delete click handler

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -27,6 +27,10 @@ export default class PortfolioForm extends Component {
     this.handleThumbDrop = this.handleThumbDrop.bind(this);
     this.handleBannerDrop = this.handleBannerDrop.bind(this);
     this.handleLogoDrop = this.handleLogoDrop.bind(this);
+
+    this.thumbRef = React.createRef();
+    this.bannerRef = React.createRef();
+    this.logoRef = React.createRef();
   }
 
   handleThumbDrop() {
@@ -101,6 +105,21 @@ export default class PortfolioForm extends Component {
       )
       .then(response => {
         this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
+
+        this.setState({
+          name: "",
+          description: "",
+          category: "eCommerce",
+          position: "",
+          url: "",
+          thumb_image: "",
+          banner_image: "",
+          logo: ""
+        });
+
+        [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
+          ref.current.dropzone.removeAllFiles();
+        });
       })
       .catch(error => {
         console.log("portfolio form handleSubmit error", error);
@@ -111,83 +130,91 @@ export default class PortfolioForm extends Component {
 
   render() {
     return (
-      <div>
-        <h1>PortfolioForm</h1>
+      <form onSubmit={this.handleSubmit} className='portfolio-form-wrapper'>
+        <div className='two-column'>
+          <input
+            type='text'
+            name='name'
+            placeholder='Portfolio Item Name'
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
 
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <input
-              type='text'
-              name='name'
-              placeholder='Portfolio Item Name'
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
+          <input
+            type='text'
+            name='url'
+            placeholder='URL'
+            value={this.state.url}
+            onChange={this.handleChange}
+          />
+        </div>
 
-            <input
-              type='text'
-              name='url'
-              placeholder='URL'
-              value={this.state.url}
-              onChange={this.handleChange}
-            />
-          </div>
+        <div className='two-column'>
+          <input
+            type='text'
+            name='position'
+            placeholder='Position'
+            value={this.state.position}
+            onChange={this.handleChange}
+          />
 
-          <div>
-            <input
-              type='text'
-              name='position'
-              placeholder='Position'
-              value={this.state.position}
-              onChange={this.handleChange}
-            />
+          <select
+            name='category'
+            value={this.state.category}
+            onChange={this.handleChange}
+            className='select-element'
+          >
+            <option value='eCommerce'>eCommerce</option>
+            <option value='Scheduling'>Scheduling</option>
+            <option value='Enterprise'>Enterprise</option>
+          </select>
+        </div>
 
-            <select
-              name='category'
-              value={this.state.category}
-              onChange={this.handleChange}
-            >
-              <option value='eCommerce'>eCommerce</option>
-              <option value='Scheduling'>Scheduling</option>
-              <option value='Enterprise'>Enterprise</option>
-            </select>
-          </div>
+        <div className='one-column'>
+          <textarea
+            type='text'
+            name='description'
+            placeholder='Description'
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </div>
 
-          <div>
-            <textarea
-              type='text'
-              name='description'
-              placeholder='Description'
-              value={this.state.description}
-              onChange={this.handleChange}
-            />
-          </div>
+        <div className='image-uploaders'>
+          <DropzoneComponent
+            ref={this.thumbRef}
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleThumbDrop()}
+          >
+            <div className='dz-message'>Thumbnail</div>
+          </DropzoneComponent>
 
-          <div className='image-uploaders'>
-            <DropzoneComponent
-              config={this.componentConfig()}
-              djsConfig={this.djsConfig()}
-              eventHandlers={this.handleThumbDrop()}
-            />
+          <DropzoneComponent
+            ref={this.bannerRef}
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleBannerDrop()}
+          >
+            <div className='dz-message'>Banner</div>
+          </DropzoneComponent>
 
-            <DropzoneComponent
-              config={this.componentConfig()}
-              djsConfig={this.djsConfig()}
-              eventHandlers={this.handleBannerDrop()}
-            />
+          <DropzoneComponent
+            ref={this.logoRef}
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleLogoDrop()}
+          >
+            <div className='dz-message'>Logo</div>
+          </DropzoneComponent>
+        </div>
 
-            <DropzoneComponent
-              config={this.componentConfig()}
-              djsConfig={this.djsConfig()}
-              eventHandlers={this.handleLogoDrop()}
-            />
-          </div>
-
-          <div>
-            <button type='submit'>Save</button>
-          </div>
-        </form>
-      </div>
+        <div>
+          <button className='btn' type='submit'>
+            Save
+          </button>
+        </div>
+      </form>
     );
   }
 }
