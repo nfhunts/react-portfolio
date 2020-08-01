c37db705502ff6f9431bf8aeafbaf33cb8c0ae11
nfhunts <nfhunts@gmail.com>
2020-08-01 10:58:28 -0600

    added delete icon

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import BlogItem from "../blog/blog-item";
-import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
   constructor() {
